20a49103ee5e421fb992af30e84875483b8d5d3c
janishiestand <janis.hiestand@gmail.com>
2024-02-09 15:38:06 +0100

    More clean up and corrections

--- FactoryForge_Frontend/src/api/API.js
+++ FactoryForge_Frontend/src/api/API.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export const API = axios.create({
+const API = axios.create({
   baseURL: "https://factoryforge-5f88b931d18d.herokuapp.com/api/",
 });
+
+export default API;
