a3d13511205c96dad63353956b8ad23b255375c2
Khilaar <luca.oliverio@hotmail.com>
2024-02-10 21:32:42 +0100

    Added filter functionality in productinventory route

--- FactoryForge_Frontend/src/Routes/ProtectedPages/ProductInventory.jsx
+++ FactoryForge_Frontend/src/Routes/ProtectedPages/ProductInventory.jsx
@@ -4,6 +4,7 @@ import API from "../../api/API";
 
 const ProductInventory = () => {
   const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   /*###########################*/
   /*Fetch all the products and save them with use state*/
@@ -21,14 +22,51 @@ const ProductInventory = () => {
   }, []);
   /*###########################*/
 
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
     <div className="inventory-background">
       <section>
         {/*###########################*/}
         {/*Products Inventory*/}
-        <h2>All Products</h2>
+
+        <div className="title-and-searchbar">
+          <h3>All Products</h3>
+
+          <span className="searchbar">
+            <h3>search</h3>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </span>
+        </div>
+        <ul className="product-list" id="sort-list">
+          {
+            <li key="sort-item" className="product-item">
+              <span>
+                <button>sort</button>
+              </span>
+              <span>
+                <button>sort</button>
+              </span>
+              <span>
+                <button>sort</button>
+              </span>
+              <span>
+                <button>sort</button>
+              </span>
+              <span>
+                <button>sort</button>
+              </span>
+            </li>
+          }
+        </ul>
         <ul className="product-list">
-          {products.map((product) => (
+          {filteredProducts.map((product) => (
             <li key={product.id} className="product-item">
               <span>id: {product.id}</span>
               <span>{product.title}</span>
