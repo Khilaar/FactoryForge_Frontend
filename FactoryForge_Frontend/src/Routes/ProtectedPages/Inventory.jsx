1b2fa3ebd988ae33418b1a0ea1eb7eb6925a23bb
janishiestand <janis.hiestand@gmail.com>
2024-02-10 23:56:44 +0100

    test

--- FactoryForge_Frontend/src/Routes/ProtectedPages/Inventory.jsx
+++ FactoryForge_Frontend/src/Routes/ProtectedPages/Inventory.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "../../styles/sass/_components.scss";
 import API from "../../api/API";
 
 const Inventory = () => {
