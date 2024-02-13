3e17621d3f1483ce706f3da6cb6706d93e7f7767
janishiestand <janis.hiestand@gmail.com>
2024-02-13 14:53:38 +0100

    Login hotfix.

--- FactoryForge_Frontend/src/Routes/Authentication/RegisterPage.jsx
+++ FactoryForge_Frontend/src/Routes/Authentication/RegisterPage.jsx
@@ -1,3 +1,67 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import API from "../../api/API.js";
+
 export default function RegisterPage() {
-  return <></>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const triggerRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await API.post();
+
+      navigate("/");
+    } catch (error) {
+      console.log("Error during login:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="login-container">
+        <form className="login-form" onSubmit={triggerRegister}>
+          <div className="input-container">
+            <i className="fi fi-rr-user" />
+            <input
+              type="text"
+              placeholder="Username"
+              autoComplete="off"
+              onChange={(e) =>
+                setFormData({ ...formData, username: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-container">
+          <i className="fi fi-rr-envelope" />
+            <input
+              type="text"
+              placeholder="Email"
+              autoComplete="off"
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="input-container">
+            <i className="fi fi-rr-lock"></i>
+            <input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
