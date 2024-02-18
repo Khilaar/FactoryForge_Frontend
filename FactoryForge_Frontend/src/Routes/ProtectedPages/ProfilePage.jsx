fb1562a36676d5ba1815b9d48d9f8c61d1864964
janishiestand <janis.hiestand@gmail.com>
2024-02-18 21:26:17 +0100

    Patching works now

--- FactoryForge_Frontend/src/Routes/ProtectedPages/ProfilePage.jsx
+++ FactoryForge_Frontend/src/Routes/ProtectedPages/ProfilePage.jsx
@@ -7,36 +7,72 @@ export default function ProfilePage() {
     username: userProfile.username || "",
     first_name: userProfile.first_name || "",
     last_name: userProfile.last_name || "",
-    avatar: userProfile.avatar || "",
     email: userProfile.email || "",
     address: userProfile.address || "",
   });
   const accessToken = localStorage.getItem("access_token");
   const [showEditProfile, setShowEditProfile] = useState(false);
+  const [newAvatar, setNewAvatar] = useState("");
   const config = {
     headers: {
       Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json",
+      "Content-Type": "multipart/form-data",
     },
   };
 
   useEffect(() => {
-    const fetchUserProfile = async () => {
-      if (!accessToken) {
-        throw new Error("Access Token not found.");
-      }
-      try {
-        const response = await API.get("users/me/", config);
-        setUserProfile(response.data);
-      } catch (error) {
-        console.error("Error fetching client orders: ", error);
-      }
-    };
     fetchUserProfile();
   }, [accessToken]);
 
+  const fetchUserProfile = async () => {
+    if (!accessToken) {
+      throw new Error("Access Token not found.");
+    }
+    try {
+      const response = await API.get("users/me/", config);
+      setUserProfile(response.data);
+      setUpdateUserProfile({
+        username: response.data.username || "",
+        first_name: response.data.first_name || "",
+        last_name: response.data.last_name || "",
+        email: response.data.email || "",
+        address: response.data.address || "",
+      });
+    } catch (error) {
+      console.error("Error fetching client orders: ", error);
+    }
+  };
+
   const toggleShowEdit = () => {
     setShowEditProfile((prevData) => !prevData);
+  };
+
+  const handleFormData = (fieldName, value) => {
+    setUpdateUserProfile({
+      ...updateUserProfile,
+      [fieldName]: value,
+    });
+    // if (fieldName === "avatar") {
+    //   console.log(value);
+    //   const reader = new FileReader();
+    //   reader.onload = (value) => {
+    //     setNewAvatar(value);
+    //   };
+    // }
+  };
+
+  const submitUpdate = async (e) => {
+    e.preventDefault();
+    if (!accessToken) {
+      throw new Error("Access Token not found.");
+    }
+    try {
+      await API.patch("users/me/update/", updateUserProfile, config);
+      toggleShowEdit();
+      fetchUserProfile();
+    } catch (error) {
+      console.error("Error updating user: ", error);
+    }
   };
 
   return (
@@ -68,13 +104,15 @@ export default function ProfilePage() {
                   <i className="fi fi-rr-marker"></i>
                   <span>{userProfile.address || "N/A"}</span>
                 </div>
-                <div title="Type of User">
-                  <i className="fi fi-rr-text"></i>
-                  <span>{userProfile.type_of_user || "N/A"}</span>
-                </div>
-                <div title="ID">
-                  <i className="fi fi-rr-hastag"></i>
-                  <span>{userProfile.id || "N/A"}</span>
+                <div style={{ gap: "15px" }}>
+                  <div title="Type of User">
+                    <i className="fi fi-rr-text"></i>
+                    <span>{userProfile.type_of_user || "N/A"}</span>
+                  </div>
+                  <div title="ID">
+                    <i className="fi fi-rr-hastag"></i>
+                    <span>{userProfile.id || "N/A"}</span>
+                  </div>
                 </div>
                 <div className="editButtonProfile">
                   <button onClick={toggleShowEdit}>EDIT</button>
@@ -86,41 +124,88 @@ export default function ProfilePage() {
               <div className="profileBox">
                 <label htmlFor="avatar-input">
                   <div className="avatar-container-update" title="Avatar">
-                    <img src={userProfile.avatar} />
+                    {newAvatar ? (
+                      <>
+                        <img src={newAvatar} alt="Avatar Preview" />
+                      </>
+                    ) : (
+                      <>
+                        <img src={userProfile.avatar} />
+                      </>
+                    )}
                   </div>
                   <input
                     type="file"
                     id="avatar-input"
                     style={{ display: "none" }}
+                    onChange={(e) =>
+                      handleFormData("avatar", e.target.files[0])
+                    }
                   />
                 </label>
                 <div className="profile-info-container-update">
                   <div className="input-container">
                     <i className="fi fi-rr-user" />
-                    <input type="text" placeholder="Username" />
+                    <input
+                      type="text"
+                      placeholder="Username"
+                      value={updateUserProfile.username}
+                      onChange={(e) =>
+                        handleFormData("username", e.target.value)
+                      }
+                    />
                   </div>
                   <div className="fullNameDiv">
                     <div className="input-container">
                       <i className="fi fi-rr-first"></i>
-                      <input type="text" placeholder="First Name" />
+                      <input
+                        type="text"
+                        placeholder="First Name"
+                        value={updateUserProfile.first_name}
+                        onChange={(e) =>
+                          handleFormData("first_name", e.target.value)
+                        }
+                      />
                     </div>
                     <div className="input-container">
                       <i className="fi fi-rr-second"></i>
-                      <input type="text" placeholder="Last Name" />
+                      <input
+                        type="text"
+                        placeholder="Last Name"
+                        value={updateUserProfile.last_name}
+                        onChange={(e) =>
+                          handleFormData("last_name", e.target.value)
+                        }
+                      />
                     </div>
                   </div>
                   <div className="input-container">
                     <i className="fi fi-rr-at"></i>
-                    <input type="email" placeholder="Email" />
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      value={updateUserProfile.email}
+                      onChange={(e) => handleFormData("email", e.target.value)}
+                    />
                   </div>
                   <div className="input-container">
                     <i className="fi fi-rr-marker"></i>
-                    <input type="text" placeholder="Address" />
+                    <input
+                      type="text"
+                      placeholder="Address"
+                      value={updateUserProfile.address}
+                      onChange={(e) =>
+                        handleFormData("address", e.target.value)
+                      }
+                    />
                   </div>
                 </div>
-                <div className="editButtonProfile">
-                  <button onClick={toggleShowEdit}>SAVE</button>
+                <div className="editButtonProfile save">
+                  <button onClick={(e) => submitUpdate(e)}>SAVE</button>
                 </div>
+                <button className="backButton" onClick={toggleShowEdit}>
+                  Back
+                </button>
               </div>
             </>
           )}
