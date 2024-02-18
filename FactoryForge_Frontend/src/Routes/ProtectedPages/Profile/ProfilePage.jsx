40f0c9f11a59175840e20270b13a9877252a6a7e
janishiestand <janis.hiestand@gmail.com>
2024-02-18 17:10:13 +0100

    Update user design

--- FactoryForge_Frontend/src/Routes/ProtectedPages/Profile/ProfilePage.jsx
+++ FactoryForge_Frontend/src/Routes/ProtectedPages/Profile/ProfilePage.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import API from "../../../api/API";
-import { useNavigate } from "react-router-dom";
 
 export default function ProfilePage() {
-  const navigate = useNavigate();
   const [userProfile, setUserProfile] = useState({});
+  const [updateUserProfile, setUpdateUserProfile] = useState({
+    username: userProfile.username || "",
+    first_name: userProfile.first_name || "",
+    last_name: userProfile.last_name || "",
+    avatar: userProfile.avatar || "",
+    email: userProfile.email || "",
+    address: userProfile.address || "",
+  });
   const accessToken = localStorage.getItem("access_token");
   const [showEditProfile, setShowEditProfile] = useState(false);
   const config = {
@@ -29,11 +35,6 @@ export default function ProfilePage() {
     fetchUserProfile();
   }, [accessToken]);
 
-  // const redirectToEdit = (e) => {
-  //   e.preventDefault();
-  //   navigate("/profile/edit");
-  // };
-
   const toggleShowEdit = () => {
     setShowEditProfile((prevData) => !prevData);
   };
@@ -42,45 +43,87 @@ export default function ProfilePage() {
     <>
       <div className="profilePage-container">
         <div className="profilePage-frame">
-          <div className="profileBox">
-            <div className="avatar-container" title="Avatar">
-              <img src={userProfile.avatar} />
+          {!showEditProfile && updateUserProfile ? (
+            <div className="profileBox">
+              <div className="avatar-container" title="Avatar">
+                <img src={userProfile.avatar} />
+              </div>
+              <div className="profile-info-container">
+                <div title="Username">
+                  <i className="fi fi-rr-user" />
+                  <span>{userProfile.username}</span>
+                </div>
+                <div title="Full Name">
+                  <i className="fi fi-rr-id-badge"></i>
+                  <span>
+                    {userProfile.first_name || "N/A"}{" "}
+                    {userProfile.last_name || "N/A"}
+                  </span>
+                </div>
+                <div title="Email">
+                  <i className="fi fi-rr-at"></i>
+                  <span>{userProfile.email || "N/A"}</span>
+                </div>
+                <div title="Address">
+                  <i className="fi fi-rr-marker"></i>
+                  <span>{userProfile.address || "N/A"}</span>
+                </div>
+                <div title="Type of User">
+                  <i className="fi fi-rr-text"></i>
+                  <span>{userProfile.type_of_user || "N/A"}</span>
+                </div>
+                <div title="ID">
+                  <i className="fi fi-rr-hastag"></i>
+                  <span>{userProfile.id || "N/A"}</span>
+                </div>
+                <div className="editButtonProfile">
+                  <button onClick={toggleShowEdit}>EDIT</button>
+                </div>
+              </div>
             </div>
-            <div className="profile-info-container">
-              <div>
-                <i className="fi fi-rr-user" />
-                <span title="Username">{userProfile.username}</span>
+          ) : (
+            <>
+              <div className="profileBox">
+                <label htmlFor="avatar-input">
+                  <div className="avatar-container-update" title="Avatar">
+                    <img src={userProfile.avatar} />
+                  </div>
+                  <input
+                    type="file"
+                    id="avatar-input"
+                    style={{ display: "none" }}
+                  />
+                </label>
+                <div className="profile-info-container-update">
+                  <div className="input-container">
+                    <i className="fi fi-rr-user" />
+                    <input type="text" placeholder="Username" />
+                  </div>
+                  <div className="fullNameDiv">
+                    <div className="input-container">
+                      <i className="fi fi-rr-first"></i>
+                      <input type="text" placeholder="First Name" />
+                    </div>
+                    <div className="input-container">
+                      <i className="fi fi-rr-second"></i>
+                      <input type="text" placeholder="Last Name" />
+                    </div>
+                  </div>
+                  <div className="input-container">
+                    <i className="fi fi-rr-at"></i>
+                    <input type="email" placeholder="Email" />
+                  </div>
+                  <div className="input-container">
+                    <i className="fi fi-rr-marker"></i>
+                    <input type="text" placeholder="Address" />
+                  </div>
+                </div>
+                <div className="editButtonProfile">
+                  <button onClick={toggleShowEdit}>SAVE</button>
+                </div>
               </div>
-              <div>
-                <i className="fi fi-rr-id-badge"></i>
-                <span title="Full Name">
-                  {userProfile.first_name || "N/A"}{" "}
-                  {userProfile.last_name || "N/A"}
-                </span>
-              </div>
-              <div>
-                <i className="fi fi-rr-at"></i>
-                <span title="Email">{userProfile.email || "N/A"}</span>
-              </div>
-              <div>
-                <i className="fi fi-rr-marker"></i>
-                <span title="Address">{userProfile.address || "N/A"}</span>
-              </div>
-              <div>
-                <i className="fi fi-rr-text"></i>
-                <span title="Type of User">
-                  {userProfile.type_of_user || "N/A"}
-                </span>
-              </div>
-              <div>
-                <i className="fi fi-rr-text"></i>
-                <span title="ID">{userProfile.id || "N/A"}</span>
-              </div>
-              <div className="editButtonProfile">
-                <button>EDIT</button>
-              </div>
-            </div>
-          </div>
+            </>
+          )}
         </div>
       </div>
     </>
