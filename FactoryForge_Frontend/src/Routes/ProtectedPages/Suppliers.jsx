9c26e3f65832739449d3698473e813aeb4c1728d
Khilaar <luca.oliverio@hotmail.com>
2024-02-21 16:48:21 +0100

    Styling changes in supplier + delete functionality changed

--- FactoryForge_Frontend/src/Routes/ProtectedPages/Suppliers.jsx
+++ FactoryForge_Frontend/src/Routes/ProtectedPages/Suppliers.jsx
@@ -5,7 +5,6 @@ const Suppliers = () => {
   const [suppliers, setSuppliers] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [showFormSupplier, setShowFormSupplier] = useState(false);
-  const [showDeleteAction, setShowDeleteAction] = useState(false);
   const [showSortPopUp, setShowSortPopUp] = useState(false);
   const [sortOption, setSortOption] = useState(null);
   const [formDataSupplier, setFormDataSupplier] = useState({
@@ -53,13 +52,9 @@ const Suppliers = () => {
     setShowFormSupplier(!showFormSupplier);
   };
 
-  const toggleDeleteAction = () => {
-    setShowDeleteAction(!showDeleteAction);
-  }
-
   const toggleSortPopUp = () => {
     setShowSortPopUp(!showSortPopUp);
-  }
+  };
 
   const handleCloseSupplierForm = () => {
     setFormDataSupplier({
@@ -95,20 +90,22 @@ const Suppliers = () => {
   const sortedSuppliers = useMemo(() => {
     let sortedList = [...suppliers];
 
-    if(sortOption) {
+    if (sortOption) {
       sortedList.sort((a, b) => {
         const fieldA = a[sortOption].toString().toLowerCase();
         const fieldB = b[sortOption].toString().toLowerCase();
         return fieldA.localeCompare(fieldB);
       });
     }
-    
+
     return sortedList.filter((supplier) =>
-      Object.values(supplier)
-        .some((field) => field && field.toString().toLowerCase().includes(searchQuery.toLowerCase()))
+      Object.values(supplier).some(
+        (field) =>
+          field &&
+          field.toString().toLowerCase().includes(searchQuery.toLowerCase()),
+      ),
     );
-    }, [suppliers, searchQuery, sortOption]);
-  
+  }, [suppliers, searchQuery, sortOption]);
 
   const handleDeleteSupplier = async (supplierId) => {
     try {
@@ -116,15 +113,15 @@ const Suppliers = () => {
       if (!accessToken) {
         throw new Error("Access token not found");
       }
-  
+
       const config = {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       };
-  
+
       await API.delete(`/suppliers/${supplierId}`, config);
-  
+
       const response = await API.get("/suppliers/");
       setSuppliers(response.data);
     } catch (error) {
@@ -136,58 +133,67 @@ const Suppliers = () => {
     <div>
       <div className="title-and-searchbar">
         <h1 className="route-title">Suppliers</h1>
-        <span className="searchbar-suppliers">  
-        <h3>Search</h3>
-        <input 
-          type="text"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        </span>
       </div>
-      
 
-      <div className="background-frame">
+      <div className="background-frame-productinventory">
+        <div className="title-and-searchbar-suppliers">
+          <button onClick={toggleSortPopUp} className="supplier-button-sort">
+            sort
+          </button>
+          <span className="searchbar-suppliers">
+            <h3>Search</h3>
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </span>
+        </div>
         <section>
           <ul>
-          <span>
-            <button onClick={toggleSortPopUp} className="supplier-button-sort">sort</button>
-          </span>
-          {showSortPopUp && (
-            <span className="sort-span">
-            <button 
-            className="sort-span-button"
-            onClick={() => {
-              setSortOption("id");
-              setShowSortPopUp(false);
-            }}
-            >id</button>
-            <button 
-            className="sort-span-button"
-            onClick={() => {
-              setSortOption("username");
-              setShowSortPopUp(false);
-            }}
-            >username</button>
-            <button 
-            className="sort-span-button"
-            onClick={() => {
-              setSortOption("first_name");
-              setShowSortPopUp(false);
-            }}
-            >first name</button>
-            <button 
-            className="sort-span-button"
-            onClick={() => {
-              setSortOption("last_name");
-              setShowSortPopUp(false);
-            }}
-            >last name</button>
-            </span>
-          )}
+            {showSortPopUp && (
+              <span className="sort-span">
+                <button
+                  className="sort-span-button"
+                  onClick={() => {
+                    setSortOption("id");
+                    setShowSortPopUp(false);
+                  }}
+                >
+                  id
+                </button>
+                <button
+                  className="sort-span-button"
+                  onClick={() => {
+                    setSortOption("username");
+                    setShowSortPopUp(false);
+                  }}
+                >
+                  username
+                </button>
+                <button
+                  className="sort-span-button"
+                  onClick={() => {
+                    setSortOption("first_name");
+                    setShowSortPopUp(false);
+                  }}
+                >
+                  first name
+                </button>
+                <button
+                  className="sort-span-button"
+                  onClick={() => {
+                    setSortOption("last_name");
+                    setShowSortPopUp(false);
+                  }}
+                >
+                  last name
+                </button>
+              </span>
+            )}
             <ul className="items-list">
               {
-                <li className="list-item-suppliers">
+                <li className="list-item-client-sort">
                   <span>
                     <p className="header-text">id</p>
                   </span>
@@ -203,11 +209,7 @@ const Suppliers = () => {
                   <span>
                     <p className="header-text">email</p>
                   </span>
-                  {showDeleteAction && (
-                    <span>
-                    <p className="header-text">Action</p>
-                  </span>
-                  )}
+                  <button>X</button>
                 </li>
               }
             </ul>
@@ -218,16 +220,9 @@ const Suppliers = () => {
                 <span>{supplier.first_name}</span>
                 <span>{supplier.last_name}</span>
                 <span>{supplier.email}</span>
-                {showDeleteAction && (
-                  <span>
-                    <button 
-                      className="supplier-delete-action-btn"
-                      onClick={() => handleDeleteSupplier(supplier.id)}
-                    >
-                      Delete
-                    </button>
-                  </span>
-                )}
+                <button onClick={() => handleDeleteSupplier(supplier.id)}>
+                  X
+                </button>
               </li>
             ))}
           </ul>
@@ -235,9 +230,6 @@ const Suppliers = () => {
         <section>
           <button className="supplier-button" onClick={toggleFormSupplier}>
             ADD
-          </button>
-          <button className="supplier-button-delete" onClick={toggleDeleteAction}>
-            DELETE
           </button>
           {showFormSupplier && (
             <div className="add-form-supply">
